ec8b74789e7775159c195c0f3715a5de69ee948f
Robin <379111601@qq.com>
2018-01-28 22:23:49 +0300

    将止损价格上调v1.0

--- app/zzsd_trade.py
+++ app/zzsd_trade.py
@@ -15,7 +15,7 @@ class Trade_Strategy:
                   'amount': 5,
                   'symbol': 'eth_usdt',
                   'size': 120,
-                  'point':1
+                  'point':0.5
                   }
         self._trader = OK_Trade(params)
 
@@ -30,12 +30,13 @@ class Trade_Strategy:
          '''
         signal = 0
         self._trader.get_calculates_values()  # 获取最新的价格，并进行相应的计算
+        #print(self._trader.new_price)
         # 当价格向上突破前五个交易单元的收盘价时，执行挂单做多
         if self._trader.new_price > np.amax(self._trader._buffer[-6:-2])+self._trader._params['point']:
             #print('(买入做多)当前价格;%s,五个交易单元高价:%s'%(self._trader._buffer[-1],np.amax(self._trader._buffer[-6:-2])))
             signal=2
         #当价格向下跌到前五个交易单元收盘高价时，平掉做多仓位止损
-        if self._trader.new_price < np.amax(self._trader._buffer[-6:-2])-self._trader._params['point']:
+        if self._trader.new_price < np.amax(self._trader._buffer[-6:-2])-self._trader._params['point']*5:
             #print('(做多平仓)当前价格;%s,五个交易单元高价:%s' % (self._trader._buffer[-1], np.amax(self._trader._buffer[-6:-2])))
             signal =1
 
@@ -44,7 +45,7 @@ class Trade_Strategy:
             #print('(买入做空)当前价格;%s,五个交易单元低价:%s' % (self._trader._buffer[-1],  np.amin(self._trader._buffer[-6:-2])))
             signal = -2
         # 当价格向上涨到前五个交易单元收盘低价时，平掉做空仓位止损
-        if self._trader.new_price > np.amin(self._trader._buffer[-6:-2])+self._trader._params['point']:
+        if self._trader.new_price > np.amin(self._trader._buffer[-6:-2])+self._trader._params['point']*5:
             #print('(做空平仓)当前价格;%s,五个交易单元低价:%s' % (self._trader._buffer[-1], np.amin(self._trader._buffer[-6:-2])))
             signal = -1
 
@@ -79,6 +80,8 @@ class Trade_Strategy:
         user_pos = self._trader.get_user_position(symbol)
         if user_pos['status']:
             signal = self.get_trade_signal(symbol, period, size, contract_type)
+            #price = self._trader.get_price_depth(symbol)
+            #print('盘口:%s,%s'%(price[0],price[0]))
             #print('交易信号:%s'%signal)
             if signal==1:#将做多持仓平仓
                 if user_pos['buy_amount'] > 0:
@@ -103,8 +106,6 @@ class Trade_Strategy:
 if __name__ == '__main__':
     ts = Trade_Strategy()
     while True:
-        try:
-            ts.trade_system('eth_usdt')
-            ts.stop_less_profit('eth_usdt')
-        except Exception as e:
-            print('发生异常:%s'%e)
+        ts.trade_system('eth_usdt')
+        ts.stop_less_profit('eth_usdt')
+
