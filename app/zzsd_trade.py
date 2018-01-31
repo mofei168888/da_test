ffbe6584a5efa51abf7b1123afa15083e043d01c
Robin <379111601@qq.com>
2018-01-31 17:40:11 +0300

    修改日志输出方式v1.2

--- app/zzsd_trade.py
+++ app/zzsd_trade.py
@@ -301,7 +301,7 @@ if __name__ == '__main__':
     zlog = logging.getLogger('ok.strategy.zzsd')
     tlog = logging.getLogger('ok.trade')
     tlog.setLevel(logging.ERROR)
-    zlog.setLevel(logging.INFO)
+    zlog.setLevel(logging.INFO)#将主机的日志改一下
     while True:
         try:
             ts.trade_kc('eth_usdt')
